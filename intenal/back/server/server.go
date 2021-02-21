369851af63301bbcf340ae6df7f08568803f7943
Aleksandr Akhapkin <sashapow1@gmail.com>
2021-02-21 15:16:58 +0300

    hotfix

--- intenal/back/server/server.go
+++ intenal/back/server/server.go
@@ -2,16 +2,18 @@ package server
 
 import (
 	"github.com/AleksandrAkhapkin/testWLine/intenal/back/server/handlers"
+	"github.com/AleksandrAkhapkin/testWLine/logger"
 	"github.com/pkg/errors"
 	"log"
 	"net/http"
 )
 
+//запускаем сервер
 func StartServer(handlers *handlers.Handlers, port string) {
 
 	router := NewRouter(handlers)
 	log.Println("Start service in port " + port)
 	if err := http.ListenAndServe(port, router); err != nil {
-		log.Fatal(errors.Wrap(err, "err with NewRouter"))
+		logger.LogFatal(errors.Wrap(err, "err with NewRouter"))
 	}
 }
